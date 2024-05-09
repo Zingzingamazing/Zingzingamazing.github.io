4ee25d1eb44fd041acb78462116d01611d331ce9
Rahkehs <shekharshrestha27@gmail.com>
2024-05-09 13:48:39 +1000

    Added log in page

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './LoginForm.css';
+import { FaRegUserCircle, } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
+const LoginForm = () => {
+  return (
+    <div className='wrapper'>
+<form action="">
+    <h1>Login</h1>
+    <div className='input-box'>
+        <input type="text" placeholder='Username' required />
+        <FaRegUserCircle className='icon' />
+    </div>
+    <div className='input-box'>
+        <input type="password" placeholder='Password' required />
+        <FaLock className='icon'/>
+    </div>
+    <div className='remember-forgot'>
+<label><input type="checkbox" />Remember me</label>
+<a href="#"> Forgot Password? </a>
+    </div>
+    <div>
+        <button type='submit'>Login</button>
+        
+        <div className='register-link'></div>
+        <p>Dont have account <a href="#">Register</a></p>
+    </div>
+
+</form>
+
+    </div>
+  )
+}
+
+export default LoginForm
